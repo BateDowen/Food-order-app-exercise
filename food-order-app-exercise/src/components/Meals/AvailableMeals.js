10c3e430c330b98d8d6bd9b377f052b0219e9e1a
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-08-11 19:16:59 +0300

    add individual items and displaying them

--- food-order-app-exercise/src/components/Meals/AvailableMeals.js
+++ food-order-app-exercise/src/components/Meals/AvailableMeals.js
@@ -1,41 +1,44 @@
-
+import CartItem from "../Cart/CartItem";
+import Card from "../UI/Card";
 import clasess from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];    
-const AvailableMeals = () =>{
-const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+const AvailableMeals = () => {
+  const mealList = DUMMY_MEALS.map((meal) => (
+    <CartItem key={meal.id} name={meal.name} price={meal.price}></CartItem>
+  ));
 
-return (
+  return (
     <section className={clasess.meals}>
-        <ul>
-            {mealList}
-        </ul>
+      <Card>
+        <ul>{mealList}</ul>
+      </Card>
     </section>
-)
+  );
 };
 export default AvailableMeals;
